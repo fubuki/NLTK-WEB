4869669d382f39682f4542502964db77a8de38e7
fubuki <fubuki@users.noreply.github.com>
2014-03-29 20:31:56 +0800

    測試nltk在中文和英文下的處理情形。

--- mongo.py
+++ mongo.py
@@ -1,8 +1,17 @@
+import nltk
 from pymongo import MongoClient
-
+from nltk.probability import ConditionalFreqDist
+from nltk.tokenize import word_tokenize
 
 client = MongoClient()
 scrapy = client.scrapy
 yahoo = scrapy.yahoo
 html = yahoo.find_one()
-print html
+
+text = nltk.word_tokenize(html['title'])
+result = nltk.pos_tag(text)
+print result
+
+text = nltk.word_tokenize("And now for something completely different")
+result = nltk.pos_tag(text)
+print result
