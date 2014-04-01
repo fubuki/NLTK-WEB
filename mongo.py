a11c907db429de8c4fc40f279e17fa3f1120c5d6
fubuki <fubuki@users.noreply.github.com>
2014-04-01 23:26:15 +0800

    對新聞標題作分析樹。

--- mongo.py
+++ mongo.py
@@ -2,13 +2,19 @@ import nltk
 from pymongo import MongoClient
 from nltk.probability import ConditionalFreqDist
 from nltk.tokenize import word_tokenize
+from nltk.corpus import treebank
 
 client = MongoClient()
 scrapy = client.scrapy
 bbc = scrapy.bbc
 html = bbc.find_one()
 
-text = nltk.word_tokenize(html['body'])
-result = nltk.pos_tag(text)
-print result
+# tokens
+tokens = nltk.word_tokenize(html['body'])
+# identify
+result = nltk.pos_tag(tokens)
 
+# parse tree
+tree = treebank.parsed_sents(html['title'])[0]
+
+tree.draw()
