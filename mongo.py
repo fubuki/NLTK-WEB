c8840afd8b8916ac83106da36e48bd74f9a92d4d
fubuki <fubuki@users.noreply.github.com>
2014-04-02 00:34:08 +0800

    測試wsj_0001.mrg

--- mongo.py
+++ mongo.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from nltk.probability import ConditionalFreqDist
 from nltk.tokenize import word_tokenize
 from nltk.corpus import treebank
+from nltk.corpus import wordnet
 
 client = MongoClient()
 scrapy = client.scrapy
@@ -15,6 +16,6 @@ tokens = nltk.word_tokenize(html['body'])
 result = nltk.pos_tag(tokens)
 
 # parse tree
-tree = treebank.parsed_sents(html['title'])[0]
+tree = treebank.parsed_sents('wsj_0001.mrg')[0]
 
 tree.draw()
